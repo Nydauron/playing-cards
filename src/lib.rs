21558aa6c4f2acdda7bc7b2edd93ded32fb87d92
Jareth Gomes <jarethgomes@gmail.com>
2022-07-05 13:03:32 -0500

    Added breaking changes disclaimer to docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 //! 
 //! `playing-cards` is a library for developing card games. Ranging from dealing cards from a
 //! deck to hand evaluations.
+//! 
+//! DISCLAIMER:
+//! This library is still in early development. A lot of features may be subject to breaking
+//! changes across minor version changes. Be sure to check the docs for updates on any changes.
 
 #![cfg_attr(feature = "unstable", feature(test))]
 
