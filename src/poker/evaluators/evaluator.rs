74f4749f45406c8ed5c677dc9f55fb974e2af5e5
Jareth Gomes <jarethgomes@gmail.com>
2023-07-17 19:07:45 -0500

    Added documentation for `Rank`, `RankResult`, `Evaluator`

--- src/poker/evaluators/evaluator.rs
+++ src/poker/evaluators/evaluator.rs
@@ -2,13 +2,18 @@ use crate::{core::Card, poker::rank::Rank};
 
 use super::{high_evaluator, EvaluatorError, low_evaluator};
 
+/// Contains all evaluator variants allowing the ability to switch between evaluators.
 #[derive(Copy, Clone, Debug)]
 pub enum Evaluator {
+    /// Enum variant representing high_evaluator
     High,
+    /// Enum variant representing low_evaluator
     Low,
 }
 
 impl Evaluator {
+    /// Calls the associated `evaluate_hand()` function based on what variant is currently
+    /// selected.
     pub fn evaluate_hand(&self, player_hand: &Vec<Card>, board: &Vec<Card>) -> Result<Vec<Rank>, EvaluatorError> {
         match self {
             Self::High => high_evaluator::evaluate_hand(player_hand, board),
--- src/poker/rank.rs
+++ src/poker/rank.rs
@@ -1,10 +1,43 @@
 use std::{cmp::Ordering, hash::Hash, collections::HashSet};
 
+/// A struct to contain hand strength metadata.
+///
+/// The `Rank` struct is fairly trasparent allowing for easy access for evaluators to create and
+/// modify values within the struct. The struct primarily stores information on hand strength, even
+/// containing information on hand rank and sub rank if the user wants to utilize those properites.
+///
+/// A provided description is also included to allow for the user to understand what the rank is in
+/// English. Typically, the `description` field is going to be the the strength of the hand, but if
+/// the evaluator fails for whatever reason, it will contain an error message instead.
+///
+/// Ranks can be compared with each other using the typical equality and inequality operations. Do
+/// note that two ranks from two different evaluators can be compared successfully which might lead
+/// to some undefined behavior in the user's implementation.
 #[derive(Debug, Clone, Eq)]
 pub struct Rank {
+    /// The strength of the `Rank`.
+    ///
+    /// This field is what is used within the implematation of `Ord`
+    /// and `Eq`.
     pub strength: u32,
+
+    /// the hand rank of the associated rank.
+    ///
+    /// Two different ranks from two different evaluators might have the same `strength`, they may
+    /// differ in `hand_rank`.
     pub hand_rank: u16,
+
+    /// The sub rank of the associated rank.
+    ///
+    /// Typically `sub_rank` is used in tandem with `hand_rank`.
     pub sub_rank: u16,
+
+    /// An optional description for the rank.
+    ///
+    /// All the provided evaluators in this library fill this field. Typically, value that is
+    /// populated is a Enlgish version of the hand made (e.g. "Trip 5s", "Kings full of 9s"). If
+    /// the evaluator fails to generate a valid description, the error string will be populated
+    /// here.
     pub description: Option<String>,
 }
 
@@ -44,18 +77,34 @@ impl PartialEq for Rank {
     }
 }
 
+/// Contains information on which key has the strongest rank (i.e the winning hand).
+///
+/// The struct does not contain any `Rank` types, but rather contains the keys associated with the
+/// rank. The underlying implementation uses HashSets to store the keys so the generic type `T`
+/// must fulfill the traits `Hash`, `Eq`, and `Copy`.
+///
+/// The vectors returned by the iterator or `hand_number(&self)` is a list of hashsets of keys. The
+/// list is sorted by strength of the key's rank with index 0 being of the strongest rank.
 #[derive(Debug, Clone)]
 pub struct RankResults<T: Hash + Eq + Copy> {
     hand_table: Vec<Vec<HashSet<T>>>,
 }
 
 impl<T: Hash + Eq + Copy> RankResults<T> {
+    /// Generates a new `RankResults<T>`.
+    ///
+    /// This does not do any other post-computation. This is typically called within the
+    /// `generate_winner_list()` functions.
     pub fn new(hand_table: Vec<Vec<HashSet<T>>>) -> Self {
         Self {
             hand_table: hand_table,
         }
     }
 
+    /// Gets the sepcified hand number if it exists.
+    ///
+    /// Similar to indexing an vector, this will either return a list of hashsets of keys which are
+    /// sorted by the strength of the key's rank, or None if the index does not exist.
     pub fn hand_number(&self, n: usize) -> Option<&Vec<HashSet<T>>> {
         self.hand_table.get(n)
     }
