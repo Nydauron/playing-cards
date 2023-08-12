09f332c831efd430df3a32d7c9156710114d822a
Jareth Gomes <jarethgomes@gmail.com>
2023-08-11 21:47:06 -0500

    Added `non_exhaustive` attribute to `EvaluatorError`

--- src/poker/evaluators/evaluator_errors.rs
+++ src/poker/evaluators/evaluator_errors.rs
@@ -9,9 +9,11 @@ fn pluralize<T: PrimInt + One>(n: T, base: &str, plural_suffix: &str) -> String 
 }
 
 /// An error wrapper that provides error handling for the evaluators
+#[non_exhaustive]
 #[derive(Debug, Eq, PartialEq, thiserror::Error)]
 pub enum EvaluatorError {
-    /// This error represents when there are not enough cards provided to the evaluator
+    /// There are not enough cards provided to the evaluator
+    #[non_exhaustive]
     #[error("{card_set_type} does not have at least {expected_count} {} (Got instead {actual_count} {})", pluralize(*.expected_count, "card", "s"), pluralize(*.actual_count, "card", "s"))]
     NotEnoughCards {
         /// What set of cards had not enough cards
@@ -21,7 +23,8 @@ pub enum EvaluatorError {
         /// The actual amount of cards recieved
         actual_count: u64,
     },
-    /// This error represents when there are too many cards provided to the evaluator
+    /// There are too many cards provided to the evaluator
+    #[non_exhaustive]
     #[error("{card_set_type} does not have at most {expected_count} {} (Got instead {actual_count} {})", pluralize(*.expected_count, "card", "s"), pluralize(*.actual_count, "card", "s"))]
     TooManyCards {
         /// What set of cards had not enough cards
@@ -32,6 +35,7 @@ pub enum EvaluatorError {
         actual_count: u64,
     },
     /// The evalautor that was called was unable to find a valid rank for the given hand
+    #[non_exhaustive]
     #[error("Failed to calculate rank based off of set of cards: {0}")]
     FailedToCalculateRank(String),
 }
